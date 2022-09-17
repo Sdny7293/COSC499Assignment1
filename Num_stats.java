03d072cf0c1883bc0547203fd3c812060ebf345b
Sydney <sdny7293@gmail.com>
2022-09-16 21:59:27 -0700

    test cases for finding max number

--- Num_stats.java
+++ Num_stats.java
@@ -1,11 +1,13 @@
 public class Num_stats {
      //method that returns maximum number of the array
      public static int findMax(int arr[]){  
-        int max = 0;  
-        for(int i = 1;i < arr.length; i++){  
-            if(max < arr[i])  
-                max = arr[i];  
+        int max=arr[0];  
+        for(int i=1;i<arr.length;i++){  
+            if(max<arr[i])  
+                max=arr[i];  
         }  
         return max;  
     }  
 }
+
+
