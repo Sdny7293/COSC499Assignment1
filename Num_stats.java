2b2a2fac2845220141aab7d49b79a7517e9574b2
Sydney <sdny7293@gmail.com>
2022-09-16 21:44:39 -0700

    proper comments

--- Num_stats.java
+++ Num_stats.java
@@ -1,5 +1,5 @@
 public class Num_stats {
-     //method that returns maximum number  
+     //method that returns maximum number of the array
      public static int findMax(int arr[]){  
         int max = 0;  
         for(int i = 1;i < arr.length; i++){  
