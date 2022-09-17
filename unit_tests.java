072280b835d148fc39e8bf9da662a8390e9d41f1
Sydney <sdny7293@gmail.com>
2022-09-16 22:04:19 -0700

    unit test update

--- unit_tests.java
+++ unit_tests.java
@@ -15,18 +15,21 @@ public class unit_tests {
     public void setUp() throws Exception {  
         System.out.println("before");  
     }  
-  
     @Test  
     public void testFindMax(){  
         System.out.println("test case find max");  
         assertEquals(9,Num_stats.findMax(new int[]{1,4,9,2}));  
         assertEquals(-4,Num_stats.findMax(new int[]{-8,-7,-4,-14}));  
     }  
+    @Test  
+    public void testCube(){  
+        System.out.println("test case cube");  
+        assertEquals(8,Num_stats.cube(2));  
+    }  
     @After  
     public void tearDown() throws Exception {  
         System.out.println("after");  
     }  
-  
     @AfterClass  
     public static void tearDownAfterClass() throws Exception {  
         System.out.println("after class");  
