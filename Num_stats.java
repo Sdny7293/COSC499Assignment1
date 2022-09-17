201711a2a46d6d9336ccd60e4512717538ad898c
Sydney <sdny7293@gmail.com>
2022-09-16 21:43:50 -0700

    method to find max number

---
+++ Num_stats.java
@@ -0,0 +1,11 @@
+public class Num_stats {
+     //method that returns maximum number  
+     public static int findMax(int arr[]){  
+        int max = 0;  
+        for(int i = 1;i < arr.length; i++){  
+            if(max < arr[i])  
+                max = arr[i];  
+        }  
+        return max;  
+    }  
+}
